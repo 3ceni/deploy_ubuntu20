653fad7be62bca9aa2ba02d2ddc3079adfa5e9c2
3ceni <147628801+3ceni@users.noreply.github.com>
2023-11-14 18:31:17 +0300

    V2.1
    Added git,

---
+++ 0git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+ echo "This script must be run as root" 
+ exit 1
+else
+ apt install git -y
+ git clone https://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@github.com/3ceni/deploy_ubuntu20.git
+ echo "git installed" >> /home/rootmt/report.txt
+ chown -R rootmt:rootmt /home/rootmt/deploy_ubuntu20
+ chmod +x /home/rootmt/deploy_ubuntu20/*.sh
+ 
+ echo "Done!!!"
+fi
--- 1start.sh
+++ 1start.sh
@@ -4,8 +4,44 @@ if [[ $EUID -ne 0 ]]; then
  echo "This script must be run as root" 
  exit 1
 else
+ magname=''
+ magnameerr='mag000'
+ magpass=''
+ DCadmin=''
+ DCadminerr='NULLadmin'
+ magdc=''
+ maglogin=''
+ tempstr=''
+ gittoken=''
+ installupdate=''
+ installsoft=''
+ installdc=''
+ installmuzlab=''
+ magname=$(ex -s +1p +q setting.txt)
+ magpass=$(ex -s +2p +q setting.txt)
+ DCadmin=$(ex -s +3p +q setting.txt)
+ magdc=$(ex -s +4p +q setting.txt)
+ maglogin=$(ex -s +5p +q setting.txt)
+ gittoken=$(ex -s +6p +q setting.txt)
+ installupdate=$(ex -s +7p +q setting.txt)
+ installsoft=$(ex -s +8p +q setting.txt)
+ installdc=$(ex -s +9p +q setting.txt)
+ installmuzlab=$(ex -s +10p +q setting.txt)
+ 
+ if [ "$magname" == "$magnameerr" ]; then
+   echo "Please, change magname setting.txt"
+   exit 1
+ fi
+ 
+ if [ $DCadmin == $DCadminerr ]; then
+   echo "Please, change DCAdmin setting.txt" 
+   exit 1
+ fi
+ echo "Setting load" >> ~/report.txt
+ 
  #set password root
  echo -e "690f326c\n690f326c\n" | passwd root
+ echo "Password root update" >> ~/report.txt
  #install ssh
  echo "Install SSH"
  apt install ssh -y
@@ -15,6 +51,7 @@ else
  sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
  service ssh restart
  systemctl enable ssh
+ echo "ssh installed" >> ~/report.txt
 
  echo "Install AnyDesk"
  #install anyDesk
@@ -22,7 +59,48 @@ else
  echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
  sudo apt update
  sudo apt install anydesk -y
- rm -f /home/rootmt/1start.sh
+ echo "AnyDesk installed" >> ~/report.txt
+ 
+ #install fonts (user participation required)
+ apt-get install ttf-mscorefonts-installer -y
+ echo "fonts installed" >> ~/report.txt
+ 
+ if ["$installupdate" == "0"]; then
+   echo "Update/Upgrade not need" 
+   exit 1
+ else
+   echo "Update Ubuntu"
+   apt update -y 
+   apt upgrade -y
+   echo "update/upgrade installed" >> ~/report.txt
+ fi
+ 
+ #set hostname
+ hostnamectl set-hostname $magname
+ #install soft
+ apt-get install -y cifs-utils
+ sudo mkdir /media/shops
+ sudo chmod 777 /media/shops
+ sudo mkdir /media/videoforto
+ sudo chmod 777 /media/videoforto
+ sudo mkdir /media/support
+ sudo chmod 777 /media/support
+ #set fstab
+ echo "//10.8.29.4/shops /media/shops cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> '/etc/fstab'
+ echo "//10.8.29.4/videoforto /media/videoforto cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
+ echo "//10.6.6.119/Distrib/Linux /media/support cifs user,username=support,password=zsedcX2019$,iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
+ echo "net folders installed" >> ~/report.txt
+  
+ if ["$installmuzlab" == "0"]; then
+    echo "Muzlab not need" 
+    exit 1
+ else
+   echo -e "690f326c\n" | su root
+   cd ~
+   wget -O - https://ftp.muzlab.ru/install.sh | bash
+   apt remove pulseaudio -y
+   echo "muzlab installed" >> ~/report.txt
+ fi
 
  echo "Done!!!"
 fi
