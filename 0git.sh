39a8f6ef16933f04b7b12eb029dc7f926189f5f3
3ceni <147628801+3ceni@users.noreply.github.com>
2023-11-15 12:11:12 +0300

    v2.21
    0git

--- 0git.sh
+++ 0git.sh
@@ -4,6 +4,7 @@ if [[ $EUID -ne 0 ]]; then
  echo "This script must be run as root" 
  exit 1
 else
+ apt update
  apt install git -y
  git clone https://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@github.com/3ceni/deploy_ubuntu20.git
  echo "git installed" >> /home/rootmt/report.txt
