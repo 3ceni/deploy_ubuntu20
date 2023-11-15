202e822a72448039b99f666668efe709729fd009
3ceni <147628801+3ceni@users.noreply.github.com>
2023-11-15 11:59:25 +0300

    v2.2
    Add "if" for foler shops, videoforto, support

--- 1start.sh
+++ 1start.sh
@@ -17,18 +17,18 @@ else
  installsoft=''
  installdc=''
  installmuzlab=''
- magname=$(ex -s +1p +q setting.txt)
- magpass=$(ex -s +2p +q setting.txt)
- DCadmin=$(ex -s +3p +q setting.txt)
- magdc=$(ex -s +4p +q setting.txt)
- maglogin=$(ex -s +5p +q setting.txt)
- gittoken=$(ex -s +6p +q setting.txt)
- installupdate=$(ex -s +7p +q setting.txt)
- installsoft=$(ex -s +8p +q setting.txt)
- installdc=$(ex -s +9p +q setting.txt)
- installmuzlab=$(ex -s +10p +q setting.txt)
+ magname=$(ex -s +15p +q setting.txt)
+ magpass=$(ex -s +16p +q setting.txt)
+ DCadmin=$(ex -s +17p +q setting.txt)
+ magdc=$(ex -s +18p +q setting.txt)
+ maglogin=$(ex -s +19p +q setting.txt)
+ gittoken=$(ex -s +20p +q setting.txt)
+ installupdate=$(ex -s +21p +q setting.txt)
+ installsoft=$(ex -s +22p +q setting.txt)
+ installdc=$(ex -s +23p +q setting.txt)
+ installmuzlab=$(ex -s +24p +q setting.txt)
  
- if [ "$magname" == "$magnameerr" ]; then
+ if [ $magname == $magnameerr ]; then
    echo "Please, change magname setting.txt"
    exit 1
  fi
@@ -37,11 +37,11 @@ else
    echo "Please, change DCAdmin setting.txt" 
    exit 1
  fi
- echo "Setting load" >> ~/report.txt
+ echo "Setting load" >> /home/rootmt/report.txt
  
  #set password root
  echo -e "690f326c\n690f326c\n" | passwd root
- echo "Password root update" >> ~/report.txt
+ echo "Password root update" >> /home/rootmt/report.txt
  #install ssh
  echo "Install SSH"
  apt install ssh -y
@@ -51,7 +51,7 @@ else
  sed -i 's/#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
  service ssh restart
  systemctl enable ssh
- echo "ssh installed" >> ~/report.txt
+ echo "ssh installed" >> /home/rootmt/report.txt
 
  echo "Install AnyDesk"
  #install anyDesk
@@ -59,20 +59,19 @@ else
  echo "deb http://deb.anydesk.com/ all main" | sudo tee /etc/apt/sources.list.d/anydesk-stable.list
  sudo apt update
  sudo apt install anydesk -y
- echo "AnyDesk installed" >> ~/report.txt
+ echo "AnyDesk installed" >> /home/rootmt/report.txt
  
  #install fonts (user participation required)
  apt-get install ttf-mscorefonts-installer -y
- echo "fonts installed" >> ~/report.txt
+ echo "fonts installed" >> /home/rootmt/report.txt
  
- if ["$installupdate" == "0"]; then
+ if [ $installupdate == 0 ]; then
    echo "Update/Upgrade not need" 
-   exit 1
  else
    echo "Update Ubuntu"
    apt update -y 
    apt upgrade -y
-   echo "update/upgrade installed" >> ~/report.txt
+   echo "update/upgrade installed" >> /home/rootmt/report.txt
  fi
  
  #set hostname
@@ -86,20 +85,40 @@ else
  sudo mkdir /media/support
  sudo chmod 777 /media/support
  #set fstab
- echo "//10.8.29.4/shops /media/shops cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> '/etc/fstab'
- echo "//10.8.29.4/videoforto /media/videoforto cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
- echo "//10.6.6.119/Distrib/Linux /media/support cifs user,username=support,password=zsedcX2019$,iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
- echo "net folders installed" >> ~/report.txt
+ numberOfString=grep -n shops /etc/fstab | cut -d: -f1
+ if [ $numberOfString >= 1 ]; then
+   echo "Folder shops is exist" >> /home/rootmt/report.txt    
+ else
+   echo "//10.8.29.4/shops /media/shops cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> '/etc/fstab'
+   echo "Folder shops add" >> /home/rootmt/report.txt
+ fi
+ 
+ numberOfString=grep -n videoforto /etc/fstab | cut -d: -f1
+ if [ $numberOfString >= 1 ]; then
+   echo "Folder videoforto is exist" >> /home/rootmt/report.txt    
+ else
+   echo "//10.8.29.4/videoforto /media/videoforto cifs user,username="$maglogin",password="$magpass",iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
+   echo "Folder videoforto add" >> /home/rootmt/report.txt
+ fi
+ 
+ numberOfString=grep -n support /etc/fstab | cut -d: -f1
+ if [ $numberOfString >= 1 ]; then
+   echo "Folder support is exist" >> /home/rootmt/report.txt    
+ else
+   echo "//10.6.6.119/Distrib/Linux /media/support cifs user,username=support,password=zsedcX2019$,iocharset=utf8,file_mode=0777,dir_mode=0777  0 0" >> /etc/fstab
+   echo "Folder support add" >> /home/rootmt/report.txt
+ fi
   
- if ["$installmuzlab" == "0"]; then
+ echo "net folders installed" >> /home/rootmt/report.txt
+  
+ if [ $installmuzlab == 0 ]; then
     echo "Muzlab not need" 
-    exit 1
  else
    echo -e "690f326c\n" | su root
    cd ~
    wget -O - https://ftp.muzlab.ru/install.sh | bash
    apt remove pulseaudio -y
-   echo "muzlab installed" >> ~/report.txt
+   echo "muzlab installed" >> /home/rootmt/report.txt
  fi
 
  echo "Done!!!"
